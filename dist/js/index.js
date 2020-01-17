2514d7b75ae9b413eb6e828fefd3e7952436075a
Hong Baojin <13809735212@163.com>
2020-01-17 16:38:39 +0800

    The anchor point has been transitioned.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 $(function () {
+  theaMsForm($('.sign-in'));
+
   function switchHandle(headEle, contEle, className) {
     $(headEle).each(function (i) {
       $(this).mouseenter(function () {
@@ -15,7 +17,19 @@ $(function () {
   $(".popup-btn").click(function () {
     $("#popup").show();
   });
+  $(".popup-btn2").click(function () {
+    $("#popup2").show();
+  });
   $('.mab-sz-close').click(function () {
     $("#popup").hide();
+    $("#popup2").hide();
+  });
+
+  // 锚点加过渡
+  $(".oz").bind("click touch", function () {
+    //根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
+    $('html,body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 50
+    }, 500);
   });
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
-$(function(){
-  function switchHandle(headEle, contEle, className){
-    $(headEle).each(function(i){
-      $(this).mouseenter(function(){
+$(function () {
+  theaMsForm($('.sign-in'));
+
+  function switchHandle(headEle, contEle, className) {
+    $(headEle).each(function (i) {
+      $(this).mouseenter(function () {
         $(this).addClass(className).siblings().removeClass(className);
         $($(contEle)[i]).addClass(className).siblings().removeClass(className);
       });
@@ -10,10 +12,24 @@ $(function(){
   switchHandle('.tab .tab-head li', '.tab .cont-item', 'cur');
 
   // 弹窗
-  $(".popup-btn").click(function() {
+  $(".popup-btn").click(function () {
     $("#popup").show();
   });
-  $('.mab-sz-close').click(function(){
+  $(".popup-btn2").click(function () {
+    $("#popup2").show();
+  });
+  $('.mab-sz-close').click(function () {
     $("#popup").hide();
+    $("#popup2").hide();
+  });
+
+  // 锚点加过渡
+  $(".oz").bind("click touch", function () {
+    //根据a标签的href转换为id选择器，获取id元素所处的位置，并高度减50px（这里根据需要自由设置）
+    $('html,body').animate(
+      {
+        scrollTop: ($($(this).attr('href')).offset().top - 50)
+      }, 500
+    );
   });
 });
